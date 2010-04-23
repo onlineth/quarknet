ce6325630acf76c91b4aec770a8cb6df01d4774d
phongn <phongn@60eacacc-2e2b-0410-ba42-dcacb69415fb>
2010-04-23 02:29:58 +0000

    Tag branches/1.3 r3106 as tags/1.3.16

---
+++ tags/1.3.16/common/src/java/gov/fnal/elab/survey/ElabSurveyQuestionAnswer.java
@@ -0,0 +1,46 @@
+package gov.fnal.elab.survey;
+
+public class ElabSurveyQuestionAnswer implements Comparable<ElabSurveyQuestionAnswer> {
+	private String text; 
+	private Integer id; 
+	private int number; 
+
+	public ElabSurveyQuestionAnswer(int id, String text, int response_no) {
+		this.id = new Integer(id);
+		this.text = text; 
+		this.number = response_no;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setId(int id) {
+		this.id = new Integer(id);
+	}
+
+	public Integer getId() {
+		return id;
+	} 
+	
+	public int compareTo(ElabSurveyQuestionAnswer o) {
+		// TODO Auto-generated method stub
+		return this.id.compareTo(o.id);
+	}
+	
+	public boolean equals(ElabSurveyQuestionAnswer o) {
+		return this.compareTo(o) == 0;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+}
