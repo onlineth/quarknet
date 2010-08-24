2b00a3d105ab973691a44eccdc9c5c56c9155c9b
phongn <phongn@60eacacc-2e2b-0410-ba42-dcacb69415fb>
2010-08-24 03:17:02 +0000

    Tag branches/2.0 r3722 as tags/2.0rc2

---
+++ tags/2.0rc2/cosmic/src/java/gov/fnal/elab/cosmic/beans/Geometries.java
@@ -0,0 +1,169 @@
+package gov.fnal.elab.cosmic.beans;
+
+import gov.fnal.elab.Elab;
+import gov.fnal.elab.ElabGroup;
+import gov.fnal.elab.ElabJspException;
+import gov.fnal.elab.cosmic.Geometry;
+import gov.fnal.elab.datacatalog.DataCatalogProvider;
+import gov.fnal.elab.util.DatabaseConnectionManager;
+import gov.fnal.elab.util.ElabException;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.TreeMap;
+
+public class Geometries implements Serializable {
+
+    private TreeMap<Integer, Geometry> geometries;
+    private HashSet<Geometry> changedGeometries;
+    private boolean readOnly;
+
+    public Geometries() {
+        this.reset();
+    }
+
+    public Geometries(int groupID, String dataDirectory, Connection c)
+            throws ElabException {
+        this.reset();
+        PreparedStatement ps = null; 
+        try {
+            ps = c.prepareStatement(
+            		"SELECT detectorid FROM research_group_detectorid " +
+    				"WHERE research_group_id= ? ORDER BY detectorid");
+            ps.setInt(1, groupID);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                addGeometry(new Geometry(dataDirectory, rs.getInt("detectorid")));
+            }
+        }
+        catch (Exception e) {
+            throw new ElabException(
+                "Problem occured when assembling geometries from database: ", e);
+        }
+        finally {
+        	DatabaseConnectionManager.close(c, ps);
+        }
+    }
+
+    public Geometries(Elab elab, int detectorID) throws ElabException {
+        this.reset();
+        addGeometry(new Geometry(elab.getProperties().getDataDir(), detectorID));
+        readOnly = true;
+    }
+
+    public Geometries(Elab elab, ElabGroup group) throws ElabException {
+        this.reset();
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = DatabaseConnectionManager
+                .getConnection(elab.getProperties());
+            ps = conn.prepareStatement(
+            		"SELECT detectorid FROM research_group_detectorid WHERE research_group_id = ? " +
+                    "ORDER BY detectorid");
+            ps.setInt(1, group.getId());
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                addGeometry(new Geometry(elab.getProperties().getDataDir(), rs
+                    .getInt("detectorid")));
+            }
+        }
+        catch (Exception e) {
+            throw new ElabException(e);
+        }
+        finally {
+            DatabaseConnectionManager.close(conn, ps);
+        }
+    }
+
+    public void reset() {
+        geometries = new TreeMap();
+        changedGeometries = new HashSet();
+    }
+
+    public void setChangedGeometries(HashSet h) {
+        changedGeometries = h;
+    }
+
+    public HashSet getChangedGeometries() {
+        return changedGeometries;
+    }
+
+    public void setGeometries(TreeMap h) {
+        geometries = h;
+    }
+
+    public TreeMap getGeometries() {
+        return geometries;
+    }
+
+    public void addGeometry(Geometry g) {
+        if (geometries != null) {
+            geometries.put(g.getDetectorID(), g);
+        }
+    }
+
+    public Geometry getGeometry(int detectorID) {
+        return geometries.get(detectorID);
+    }
+
+    public void addGeoEntry(int detectorID, GeoEntryBean geb) throws ElabJspException {
+        Geometry g = geometries.get(detectorID);
+        if (g == null) {
+            throw new ElabJspException("You are not allowed to modify this detector configuration");
+        }
+        g.addGeoEntry(geb);
+        changedGeometries.add(g);
+    }
+
+    public void removeGeoEntry(int detectorID, GeoEntryBean geb) {
+        Geometry g = geometries.get(detectorID);
+        g.removeGeoEntry(geb);
+        changedGeometries.add(g);
+    }
+
+    public GeoEntryBean getGeoEntry(int detectorID, String jd) {
+        return geometries.get(detectorID).getGeoEntry(jd);
+    }
+
+    public Iterator iterator() {
+        return geometries.values().iterator();
+    }
+
+    public Collection getValues() {
+        return geometries.values();
+    }
+
+    public void commit() throws ElabException {
+        if (readOnly) {
+            throw new ElabException("You are not allowed to modify this detector configuration");
+        }
+        for (Geometry g : changedGeometries) {
+        	g.commit(); 
+        }
+    }
+
+    public void updateMetadata(DataCatalogProvider dcp, GeoEntryBean geoEntry)
+            throws ElabException {
+        for (Geometry g : changedGeometries) {
+        	g.updateMetadata(dcp, geoEntry);
+        }
+    }
+
+    public String dump() {
+        StringBuilder sb = new StringBuilder();
+        for (Geometry g : geometries.values()) {
+        	sb.append("detectorID: ");
+        	sb.append(g.getDetectorID());
+        	sb.append("<br>"); 
+        }
+        return sb.toString();
+    }
+}
---
+++ tags/2.0rc2/cosmic/src/jsp/library/site-help.jsp
@@ -0,0 +1,136 @@
+<%@ include file="../include/elab.jsp" %>
+<%@ include file="../login/login-required.jsp" %>
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Cosmic e-Lab Tips</title>
+		<link rel="stylesheet" type="text/css" href="../css/style2.css"/>
+		<link rel="stylesheet" type="text/css" href="../css/library.css"/>
+		
+	</head>
+	
+	<body id="resources" class="library">
+		<!-- entire page container -->
+		<div id="container">
+			<div id="top">
+				<div id="header">
+					<%@ include file="../include/header.jsp" %>
+					<%@ include file="../include/nav-rollover.jspf" %>
+				</div>
+			</div>
+
+			<div id="content">
+				
+<h1>Site Tips: Use these tips to help you use the e-Lab.</h1>
+
+<table border="0" id="main">
+	<tr>
+		
+		<td>
+			<div id="center">
+				<h2>How to use the website.</h2>
+				<dl>
+					<dt>Log in/Log out:</dt>
+					<dd>Check the upper right hand corner to see the current status.</dd>
+					
+					<dt>Getting Around:</dt>
+					<dd>
+						Use the navigation bar.
+						<p>
+							<img src="../graphics/nav.jpg">
+						</p>
+						If you have upload privileges, your navigation bar will look like this:<p>
+							<img src="../graphics/nav-home-upload.gif">
+						</p>
+
+						<p>Most menu items have submenus. When you roll over the navigation bar, 
+						all the submenus show up. Here is how it looks when you roll over "Library" on the Project Map page.</p>
+						<p>	<img src="../graphics/menu-rollover.gif"/>
+						</p>
+						<p>
+							<a target="map" href="../site-index/site-map-anno.jsp?display=static">Navigation Overview</a>
+						</p>
+					</dd>
+					
+					<dt>Special icons and links:</dt>
+					<dd>
+						Click on these.
+						<p>
+							<img src="../graphics/question.gif"> and links in the text for explanations 
+							of terms in the glossary and variables in the analyses.
+						</p>
+						<p>
+ 							<img src="../graphics/Tright.gif"> and <img src="../graphics/Tdown.gif"> 
+							to show and hide analysis controls.
+						</p>
+					</dd>
+					
+					<dt>Popup Windows:</dt>
+					<dd>Be sure that you are not blocking popup windows in your browser.</dd>
+
+					<dt>Resources:</dt>
+					<dd>Explore tutorials, online resources, animations and contacts.</dd>
+					
+					<dt>Project Milestones:</dt>
+					<dd>
+						<p>
+							Concepts you need to know. Skills you need to use. Tasks you need to accomplish. Use the Project Map on the home page to guide your research. </p><p>Think of this map as a subway map with one main line and four branch lines.  Along the main line are stops, milestone seminars, opportunities to check how the work is going. Off each main stop are branch lines where each stop is a project milestone. The branch lines include: <b>The Basics</b>, <b>Getting Started</b>, <b>Figure It Out</b> and <b>Tell others</b>.</p><p>For review, go through the milestones in <b>The Basics</b>. Work your way through the list of milestones in the Project Map. Click the milestones to see the references associated with each milestone.</p> 
+
+	
+                        <p>	<img src="../graphics/project-map.gif"/>
+
+						<p>
+							A <a href="milestones.jsp">text version</a> of the project milestones is available with a link below the Project Map. To access pop-up references associated with milestones in the text version, click on <img src="../graphics/ref.gif">.
+						</p>
+						
+						<p>
+							For review, go through the milestones in The Basics.
+						</p>
+
+						<p>
+							
+						</p>
+					</dd>
+
+					<dt>Log Book:</dt>
+					<dd>
+						Click on the pencil in the upper right hand corner of the banner to get to your logbook. <img src="../graphics/logbook-blk-bg.png"/>
+						<p>
+							<img src="../graphics/logbook_pencil.gif"> and "<strong>Log it!</strong>"
+							to add notes to your log book related to the milestones.
+						</p>
+						
+						<p>
+							<img src="../graphics/logbook_view_comments_small.gif"> to access teacher comments in your logbook.
+						</p>
+					</dd>
+					<dt>Notifications:</dt>
+					<dd>
+						Click on the envelopes on the upper right hand corner of the banner to see your notifications. <img src="../graphics/notification-icon.png"/>
+						<p>
+							Your teacher can send you notifications and you will also get them if one
+							of your data analyses is completed. A number in red will indicate the number of notifications you have.  If it is missing, you have none. 
+						</p>
+						
+					</dd>
+				</dl>
+			</div>
+		</td>
+		
+	</tr>
+</table>
+
+
+			</div>
+			<!-- end content -->	
+		
+			<div id="footer">
+			</div>
+		</div>
+		<!-- end container -->
+	</body>
+</html>
